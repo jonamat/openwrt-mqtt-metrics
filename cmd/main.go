51481fc0a5d29699a634a6c3dc51838e77c0c2c0
Jonathan Mataloni <jo.mataloni@gmail.com>
2025-03-04 10:55:33 +0100

    add state smd

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -63,13 +64,14 @@ type OpenWRTModemStatus struct {
 		Iccid        string `json:"iccid"`
 		SimCount     int    `json:"sim_count"`
 		ServiceModes struct {
-			Lte []string `json:"4G"`
+			Mode4G []string `json:"4G"`
+			Mode3G []string `json:"3G"`
 		} `json:"service_modes"`
 		Builtin bool   `json:"builtin"`
 		CellId  string `json:"cellid"`
 
-		Ipv6     bool     `json:"ipv6"`
-		CaSignal []string `json:"ca_signal"`
+		Ipv6 bool `json:"ipv6"`
+		// CaSignal []string `json:"ca_signal"`
 
 		BaudRate       int    `json:"baudrate"`
 		Mode           int    `json:"mode"`
@@ -101,12 +103,125 @@ type OpenWRTLoginRequestBody struct {
 
 var lastStatus = "ALIVE"
 
+type ModemLoginData struct {
+	Url      string
+	Username string
+	Password string
+}
+
+func publishStatus(mqttClient mqtt.Client, publishTopic string, modemStatus OpenWRTModemStatus) {
+	// Connection
+	mqttClient.Publish(publishTopic+"/state", 0, false, modemStatus.Data[0].State)
+	mqttClient.Publish(publishTopic+"/connectionType", 0, false, modemStatus.Data[0].ConnType)
+	mqttClient.Publish(publishTopic+"/netState", 0, false, modemStatus.Data[0].NetState)
+	mqttClient.Publish(publishTopic+"/band", 0, false, modemStatus.Data[0].Band)
+	mqttClient.Publish(publishTopic+"/ntype", 0, false, modemStatus.Data[0].NType)
+
+	// Signal
+	mqttClient.Publish(publishTopic+"/mobile_signal", 0, false, fmt.Sprintf("%d", modemStatus.Data[0].Signal))
+	mqttClient.Publish(publishTopic+"/mobile_rsrp", 0, false, fmt.Sprintf("%d", modemStatus.Data[0].Rsrp))
+	mqttClient.Publish(publishTopic+"/mobile_rsrq", 0, false, fmt.Sprintf("%d", modemStatus.Data[0].Rsrq))
+	mqttClient.Publish(publishTopic+"/mobile_sinr", 0, false, fmt.Sprintf("%d", modemStatus.Data[0].Sinr))
+
+	// Cell
+	mqttClient.Publish(publishTopic+"/cell_id", 0, false, modemStatus.Data[0].CellId)
+	mqttClient.Publish(publishTopic+"/mcc", 0, false, modemStatus.Data[0].CellInfo[0].Mcc)
+	mqttClient.Publish(publishTopic+"/mnc", 0, false, modemStatus.Data[0].CellInfo[0].Mnc)
+	mqttClient.Publish(publishTopic+"/lac", 0, false, modemStatus.Data[0].Lac)
+
+	// Sim and ISP
+	mqttClient.Publish(publishTopic+"/provider", 0, false, modemStatus.Data[0].Provider)
+	mqttClient.Publish(publishTopic+"/operator", 0, false, modemStatus.Data[0].Oper)
+	mqttClient.Publish(publishTopic+"/simState", 0, false, modemStatus.Data[0].SimState)
+	mqttClient.Publish(publishTopic+"/band_agg", 0, false, modemStatus.Data[0].ScBandAv)
+	mqttClient.Publish(publishTopic+"/service_modes_3g", 0, false, strings.Join(modemStatus.Data[0].ServiceModes.Mode3G, ","))
+	mqttClient.Publish(publishTopic+"/service_modes_4g", 0, false, strings.Join(modemStatus.Data[0].ServiceModes.Mode4G, ","))
+}
+
 func heatbeatRoutine(mqttClient mqtt.Client, topic string, lastStatus string) {
 	for {
 		mqttClient.Publish(topic, 0, false, lastStatus)
-
 		time.Sleep(time.Second)
 	}
+}
+
+func getModemData(modemLoginData ModemLoginData) (OpenWRTModemStatus, error) {
+	loginReqBody := OpenWRTLoginRequestBody{
+		Username: modemLoginData.Username,
+		Password: modemLoginData.Password,
+	}
+	loginReqBodyBytes, err := json.Marshal(loginReqBody)
+	if err != nil {
+		return OpenWRTModemStatus{}, fmt.Errorf("login request body coding failed: %s", err.Error())
+	}
+
+	loginReq, err := http.NewRequest("POST", modemLoginData.Url+"/api/login", bytes.NewBuffer(loginReqBodyBytes))
+	if err != nil {
+		return OpenWRTModemStatus{}, fmt.Errorf("login request creation failed: %s", err.Error())
+	}
+
+	loginReq.Header.Set("Content-Type", "application/json")
+	loginReq.Header.Set("Accept", "application/json")
+
+	client := &http.Client{}
+	loginRes, err := client.Do(loginReq)
+	if err != nil {
+		return OpenWRTModemStatus{}, fmt.Errorf("login request call failed: %s", err.Error())
+	}
+	defer loginRes.Body.Close()
+	if loginRes.StatusCode != 200 {
+		loginRes.Body.Close()
+		return OpenWRTModemStatus{}, fmt.Errorf("login request status code: %s", loginRes.Status)
+	}
+
+	var loginResponseBody OpenWRTLoginResponse
+	json.NewDecoder(loginRes.Body).Decode(&loginResponseBody)
+
+	statusReq, err := http.NewRequest("GET", modemLoginData.Url+"/api/mobile/modems/status_full/", nil)
+	if err != nil {
+		println("Error during stauts request creation", err.Error())
+		return OpenWRTModemStatus{}, fmt.Errorf("status request creation failed: %s", err.Error())
+	}
+	statusReq.Header.Set("Content-Type", "application/json")
+	statusReq.Header.Set("Authorization", "Bearer "+loginResponseBody.Token)
+
+	statusRes, err := client.Do(statusReq)
+	if err != nil {
+		return OpenWRTModemStatus{}, fmt.Errorf("status request call failed: %s", err.Error())
+	}
+	defer statusRes.Body.Close()
+	if statusRes.StatusCode != 200 {
+		lastStatus = "ERROR"
+		return OpenWRTModemStatus{}, fmt.Errorf("status request status code: %s", statusRes.Status)
+	}
+
+	var modemStatus OpenWRTModemStatus
+	err = json.NewDecoder(statusRes.Body).Decode(&modemStatus)
+	if err != nil {
+		return OpenWRTModemStatus{}, fmt.Errorf("status request decoding failed: %s", err.Error())
+	}
+	if !modemStatus.Success {
+		lastStatus = "ERROR"
+		return OpenWRTModemStatus{}, fmt.Errorf("status request failed by router internally")
+	}
+
+	return modemStatus, nil
+}
+
+func commandsRoutine(mqttClient mqtt.Client, PUBLISH_TOPIC string, modemLoginData ModemLoginData) {
+	mqttClient.Subscribe(PUBLISH_TOPIC+"/set", 0, func(client mqtt.Client, msg mqtt.Message) {
+		println("Received message on topic", msg.Topic(), "with payload", string(msg.Payload()))
+
+		if string(msg.Payload()) == "STATE" {
+			modemData, err := getModemData(modemLoginData)
+			if err != nil {
+				println("Error in router request:", err.Error())
+				return
+			}
+
+			publishStatus(mqttClient, PUBLISH_TOPIC, modemData)
+		}
+	})
 }
 
 func main() {
@@ -123,6 +238,12 @@ func main() {
 	var PUBLISH_TOPIC = os.Getenv("PUBLISH_TOPIC")
 	var HEARTBEAT_TOPIC = os.Getenv("HEARTBEAT_TOPIC")
 
+	modemLoginData := ModemLoginData{
+		Url:      OPENWRT_URL,
+		Username: OPENWRT_USER,
+		Password: OPENWRT_PASSWORD,
+	}
+
 	println("Starting...")
 
 	opts := mqtt.NewClientOptions().AddBroker(MQTT_BRORKER_URL)
@@ -130,6 +251,7 @@ func main() {
 	opts.ConnectRetryInterval = 5 * time.Second
 	mqttClient := mqtt.NewClient(opts)
 
+	// Connect to the MQTT broker
 	for {
 		if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
 			lastStatus = "ERROR"
@@ -142,84 +264,20 @@ func main() {
 	}
 
 	go heatbeatRoutine(mqttClient, HEARTBEAT_TOPIC, lastStatus)
+	go commandsRoutine(mqttClient, PUBLISH_TOPIC, modemLoginData)
 
+	// Loop to get the modem status periodically
 	for {
 		println("Starting loop")
 		time.Sleep(time.Duration(DELAY_SECONDS) * time.Second)
 
-		loginReqBody := OpenWRTLoginRequestBody{
-			Username: OPENWRT_USER,
-			Password: OPENWRT_PASSWORD,
-		}
-		loginReqBodyBytes, err := json.Marshal(loginReqBody)
+		modemStatus, err := getModemData(modemLoginData)
 		if err != nil {
-			println("Error during login request body marshalling", err.Error())
-			lastStatus = "ERROR"
+			println("Error in router request:", err.Error())
 			continue
 		}
 
-		loginReq, err := http.NewRequest("POST", OPENWRT_URL+"/api/login", bytes.NewBuffer(loginReqBodyBytes))
-		if err != nil {
-			println("Error during login request creation", err.Error())
-			lastStatus = "ERROR"
-			continue
-		}
-
-		loginReq.Header.Set("Content-Type", "application/json")
-		loginReq.Header.Set("Accept", "application/json")
-
-		client := &http.Client{}
-		loginRes, err := client.Do(loginReq)
-		if err != nil {
-			println("Error during login request", err.Error())
-			lastStatus = "ERROR"
-			continue
-		}
-		if loginRes.StatusCode != 200 {
-			println("Status code returned from login request", loginRes.Status)
-			lastStatus = "ERROR"
-			continue
-		}
-
-		var loginResponseBody OpenWRTLoginResponse
-		json.NewDecoder(loginRes.Body).Decode(&loginResponseBody)
-
-		statusReq, err := http.NewRequest("GET", OPENWRT_URL+"/api/mobile/modems/status_full/", nil)
-		if err != nil {
-			println("Error during stauts request creation", err.Error())
-			lastStatus = "ERROR"
-			continue
-		}
-		statusReq.Header.Set("Content-Type", "application/json")
-		statusReq.Header.Set("Authorization", "Bearer "+loginResponseBody.Token)
-
-		statusRes, err := client.Do(statusReq)
-		if err != nil {
-			println("Error during status request", err.Error())
-			lastStatus = "ERROR"
-			continue
-		}
-		if statusRes.StatusCode != 200 {
-			println("Status code returned from status request", statusRes.Status)
-			lastStatus = "ERROR"
-			continue
-		}
-
-		var modemStatus OpenWRTModemStatus
-		json.NewDecoder(statusRes.Body).Decode(&modemStatus)
-		if modemStatus.Success == false {
-			println("Status request failed on router", err.Error())
-			lastStatus = "ERROR"
-			continue
-		}
-
-		mqttClient.Publish(PUBLISH_TOPIC+"/mobile_signal", 0, false, fmt.Sprintf("%d", modemStatus.Data[0].Signal))
-		mqttClient.Publish(PUBLISH_TOPIC+"/mobile_rsrp", 0, false, fmt.Sprintf("%d", modemStatus.Data[0].Rsrp))
-		mqttClient.Publish(PUBLISH_TOPIC+"/band", 0, false, modemStatus.Data[0].Band)
-		mqttClient.Publish(PUBLISH_TOPIC+"/ntype", 0, false, modemStatus.Data[0].NType)
-
-		statusRes.Body.Close()
-		loginRes.Body.Close()
+		publishStatus(mqttClient, PUBLISH_TOPIC, modemStatus)
 
 		lastStatus = "ALIVE"
 	}
